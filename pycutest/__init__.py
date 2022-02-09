d58cfd3da5116fb4835bb7517edfa7fc9d6753ee
Lindon Roberts <lindon.roberts@anu.edu.au>
2022-02-09 15:48:34 +1100

    Add cache to PYTHONPATH at runtime (avoids need to update in bashrc during installation). Fixes #12

--- pycutest/__init__.py
+++ pycutest/__init__.py
@@ -16,7 +16,7 @@ from .problem_class import CUTEstProblem
 __all__ += ['CUTEstProblem']
 
 # When PyCUTEst is imported, run some basic installation checks
-from .system_paths import check_platform, get_cutest_path, get_sifdecoder_path, get_mastsif_path
+from .system_paths import check_platform, get_cutest_path, get_sifdecoder_path, get_mastsif_path, get_cache_path
 
 check_platform()
 get_cutest_path()  # don't care about the output for now, just want to see if it raises errors
@@ -29,3 +29,12 @@ if not 'PYCUTEST_CACHE' in os.environ:
 else:
     if not os.path.isdir(os.environ['PYCUTEST_CACHE']):
         raise ImportError("Cache directory %s does not exist" % os.environ['PYCUTEST_CACHE'])
+
+
+# Add cache to PYTHONPATH at runtime (avoids need to do this upon package installation)
+import sys
+try:
+    # Don't add to path more than once
+    sys.path.index(get_cache_path())
+except ValueError:
+    sys.path.append(get_cache_path())
