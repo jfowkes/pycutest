4886c505894ff3a72b0053bd6ebc2333e32f2d49
Jaroslav Fowkes <jaroslav.fowkes@maths.ox.ac.uk>
2023-02-21 15:05:04 +0000

    Remove from future imports in unit tests

--- pycutest/tests/test_basic_functionality.py
+++ pycutest/tests/test_basic_functionality.py
@@ -1,6 +1,3 @@
-# Ensure compatibility with Python 2
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import math as ma
 import numpy as np
 import pycutest
--- pycutest/tests/test_sparse_functionality.py
+++ pycutest/tests/test_sparse_functionality.py
@@ -1,6 +1,3 @@
-# Ensure compatibility with Python 2
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 import numpy as np
 import pycutest
 import unittest
