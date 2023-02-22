99e8342f44869bc1a271a3b288d006f68fd54037
Jaroslav Fowkes <jaroslav.fowkes@maths.ox.ac.uk>
2023-02-22 09:58:32 +0000

    Use asserts in unit tests for multiple instances

--- pycutest/tests/test_multiple_instances.py
+++ pycutest/tests/test_multiple_instances.py
@@ -1,7 +1,11 @@
+import numpy as np
 import pycutest
 import unittest
 
 # All problems used here: ROSENBR, ARGLALE
+
+def array_compare(x, y, thresh=1e-8):
+    return np.max(np.abs(x - y)) < thresh
 
 class testCUTEstProblemInstances(unittest.TestCase):
     def runTest(self):
@@ -11,24 +15,24 @@ class testCUTEstProblemInstances(unittest.TestCase):
 
         # test re-import of same problem
         p = pycutest.import_problem('ROSENBR')
-        print(p.x0)
+        self.assertTrue(array_compare(p.x0, np.array([-1.2,1.])), msg="Wrong x0")
         p = pycutest.import_problem('ROSENBR')
-        print(p.x0)
+        self.assertTrue(array_compare(p.x0, np.array([-1.2,1.])), msg="Wrong x0")
         del p
 
         # test re-import of same problem after delete
         p = pycutest.import_problem('ROSENBR')
-        print(p.x0)
+        self.assertTrue(array_compare(p.x0, np.array([-1.2,1.])), msg="Wrong x0")
         p = pycutest.import_problem('ROSENBR')
-        print(p.x0)
+        self.assertTrue(array_compare(p.x0, np.array([-1.2,1.])), msg="Wrong x0")
 
         # test re-import of problem with parameters
         p2 = pycutest.import_problem('ARGLALE', sifParams={'N':10})
-        print(p2.x0)
+        self.assertTrue(array_compare(p2.x0, np.ones((10,))), msg="Wrong x0")
         p2 = pycutest.import_problem('ARGLALE', sifParams={'N':10})
-        print(p2.x0)
+        self.assertTrue(array_compare(p2.x0, np.ones((10,))), msg="Wrong x0")
         del p2
         p2 = pycutest.import_problem('ARGLALE', sifParams={'N':10})
-        print(p2.x0)
+        self.assertTrue(array_compare(p2.x0, np.ones((10,))), msg="Wrong x0")
         p2 = pycutest.import_problem('ARGLALE', sifParams={'N':10})
-        print(p2.x0)
+        self.assertTrue(array_compare(p2.x0, np.ones((10,))), msg="Wrong x0")
