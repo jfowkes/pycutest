f556f8f1f0e439c62faf20cb08323d155a7d29bc
Jari <jfowkes@users.noreply.github.com>
2025-09-29 13:01:23 +0100

    Compile CUTEst problems with -O2

--- pycutest/build_interface.py
+++ pycutest/build_interface.py
@@ -118,7 +118,7 @@ def prepare_cache(cachedName, sifParams=None):
 def decode_and_compile_problem(problemName, destination=None, sifParams=None, sifOptions=None, quiet=True):
     """
     Call sifdecode on given problem and compile the resulting .f files.
-    Use gfortran with ``-fPIC`` option for compiling.
+    Use gfortran with ``-fPIC`` and ``-O2`` options for compiling.
     Collect the resulting object file names and return them.
     This function is OS dependent. Currently works only for Linux and MacOS.
 
@@ -220,7 +220,7 @@ def decode_and_compile_problem(problemName, destination=None, sifParams=None, si
 
     # Compile FORTRAN files
     for filename in filelist:
-        cmd=['gfortran', '-fPIC', '-c', filename]
+        cmd=['gfortran', '-fPIC', '-O2', '-c', filename]
         if not quiet:
             for s in cmd:
                 print(s, end=' ')
